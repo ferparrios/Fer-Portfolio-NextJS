e54193116c3805429e4287b556c75e9abaa54988
Fernando Paredes Rios <fprios112@gmail.com>
2022-04-10 22:07:37 -0500

    fixing the position on scroll

--- pages/xexe.tsx
+++ pages/xexe.tsx
@@ -4,7 +4,11 @@ import { Layout } from "../components/Layout/Layout";
 const xexe = () => {
   return (
 
-      <div style={{color: "white"}}>a b c d efghijklmnopqrstuvwxyz</div>
+      <div style={{color: "white"}}>
+        a b c d efghijklmnopqrstuvwxyz
+        1234567890
+        abcdefghijklmn
+      </div>
 
   );
 };
