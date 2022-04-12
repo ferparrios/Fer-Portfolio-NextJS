68888f5a8bd0f68a859c7078156ae693b83201ca
Fernando Paredes Rios <fprios112@gmail.com>
2022-04-12 16:13:35 -0500

    adding skeleton on profile home

--- components/ProfileCard/ProfileCard.tsx
+++ components/ProfileCard/ProfileCard.tsx
@@ -3,7 +3,7 @@ import { es } from "i18n/es";
 import { fr } from "i18n/fr";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useProfileCard } from "./ProfileCardHooks";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
@@ -12,40 +12,70 @@ export const ProfileCard = () => {
   const { profileImage } = useProfileCard();
 
   const { locale, locales, defaultLocale } = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
 
   return (
     <div className="profile-card">
-      <div className="profile-card-image-container">
-        <img src={profileImage} className="profile-card-image" alt="..." />
-      </div>
+      {loading ? (
+        <div className="skeleton"></div>
+      ) : (
+        <div className="profile-card-image-container">
+          <img src={profileImage} className="profile-card-image" alt="..." />
+        </div>
+      )}
+
       <div className="profile-card-text-container">
-        <h1 className="profile-card-title">
-          {[
-            locale === "en-US" && en.profileTitle,
-            locale === "es-PE" && es.profileTitle,
-            locale === "fr-CA" && fr.profileTitle,
-          ]}
-        </h1>
-        <p className="profile-card-text">
-          {[
-            locale === "en-US" && en.profileDescription,
-            locale === "es-PE" && es.profileDescription,
-            locale === "fr-CA" && fr.profileDescription,
-          ]}
-        </p>
+        {loading ? (
+          <>
+            <div className="skeleton-text"></div>
+            <div className="skeleton-presentation-text"></div>
+            <div className="skeleton-presentation-text"></div>
+            <div className="skeleton-presentation-text"></div>
+            <div className="skeleton-presentation-text"></div>
+            <div className="skeleton-presentation-text"></div>
+            <div className="skeleton-presentation-text"></div>
+          </>
+        ) : (
+          <>
+            <h1 className="profile-card-title">
+              {[
+                locale === "en-US" && en.profileTitle,
+                locale === "es-PE" && es.profileTitle,
+                locale === "fr-CA" && fr.profileTitle,
+              ]}
+            </h1>
+            <p className="profile-card-text">
+              {[
+                locale === "en-US" && en.profileDescription,
+                locale === "es-PE" && es.profileDescription,
+                locale === "fr-CA" && fr.profileDescription,
+              ]}
+            </p>
+          </>
+        )}
       </div>
 
-      <div className="profile-card-button-container">
-        <div className="profile-card-button">
-          <Link href="#blog-home">
-            <p className="profile-card-text">
-              Check my content &nbsp;
-              <FontAwesomeIcon icon={faChevronRight} />
-              <FontAwesomeIcon icon={faChevronRight} />
-            </p>
-          </Link>
+      {loading ? (
+        <div className="skeleton-profile-button"></div>
+      ) : (
+        <div className="profile-card-button-container">
+          <div className="profile-card-button">
+            <Link href="#blog-home">
+              <p className="profile-card-text">
+                Check my content &nbsp;
+                <FontAwesomeIcon icon={faChevronRight} />
+                <FontAwesomeIcon icon={faChevronRight} />
+              </p>
+            </Link>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
