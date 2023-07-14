8442d9766c5e1754ff52040ef6f1599624b73e1e
Fernando Paredes Rios <fprios112@gmail.com>
2023-07-13 23:13:59 -0500

    adding mobile menu

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { en } from "../../i18n/en";
 import { es } from "../../i18n/es";
 import { fr } from "../../i18n/fr";
+import { ModalMenu } from "@components/ModalMenu/ModalMenu";
 
 export const Navbar = () => {
   const { locale } = useRouter();
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <>
       <nav
@@ -15,7 +17,7 @@ export const Navbar = () => {
         aria-label="main navigation"
       >
         <div className="navbar-brand">
-          <Link href="#">
+          <Link href="/">
             <a className="navbar-item">
               <img src="images/fer-avatar.jpg" width="20px" height="100%" />
             </a>
@@ -27,12 +29,14 @@ export const Navbar = () => {
             aria-label="menu"
             aria-expanded="false"
             data-target="navbarBasicExample"
+            onClick={() => setShowMenu(!showMenu)}
           >
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
           </a>
         </div>
+        {showMenu && <ModalMenu setShowMenu={setShowMenu} />}
 
         <div id="navbarBasicExample" className="navbar-menu">
           <div className="navbar-start">
@@ -42,52 +46,33 @@ export const Navbar = () => {
 
             <Link href={"#about"}>
               <a className="navbar-item">
-              {[
-                locale === "es-PE" && es.acerca,
-                locale === "en-US" && en.aboutMe,
-                locale === "fr-CA" && fr.sur,
-              ]}
+                {[
+                  locale === "es-PE" && es.acerca,
+                  locale === "en-US" && en.aboutMe,
+                  locale === "fr-CA" && fr.sur,
+                ]}
               </a>
             </Link>
 
             <Link href={"#portfolio"}>
               <a className="navbar-item">
-              {[
-                locale === "es-PE" && es.portfolioMenu,
-                locale === "en-US" && en.portfolioMenu,
-                locale === "fr-CA" && fr.portfolioMenu,
-              ]}
+                {[
+                  locale === "es-PE" && es.portfolioMenu,
+                  locale === "en-US" && en.portfolioMenu,
+                  locale === "fr-CA" && fr.portfolioMenu,
+                ]}
               </a>
             </Link>
 
             <Link href={"#contact"}>
               <a className="navbar-item">
-              {[
-                locale === "es-PE" && es.contactMenu,
-                locale === "en-US" && en.contactMenu,
-                locale === "fr-CA" && fr.contactMenu,
-              ]}
+                {[
+                  locale === "es-PE" && es.contactMenu,
+                  locale === "en-US" && en.contactMenu,
+                  locale === "fr-CA" && fr.contactMenu,
+                ]}
               </a>
             </Link>
-
-            {/* <div className="navbar-item has-dropdown is-hoverable">
-        <a className="navbar-link">
-          Lenguaje
-        </a>
-
-        <div className="navbar-dropdown">
-          <a className="navbar-item">
-            EN
-          </a>
-          <a className="navbar-item">
-            ES
-          </a>
-          <a className="navbar-item">
-            FR
-          </a>
-          
-        </div>
-      </div> */}
           </div>
 
           <div className="navbar-item has-dropdown is-hoverable">
@@ -111,23 +96,8 @@ export const Navbar = () => {
               </Link>
             </div>
           </div>
-
-          {/* <div className="navbar-end">
-      <div className="navbar-item">
-        <div className="buttons">
-          <a className="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a className="button is-light">
-            Log in
-          </a>
-        </div>
-      </div>
-    </div> */}
         </div>
       </nav>
     </>
   );
 };
-
-// export default Navbar
